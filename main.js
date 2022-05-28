85302e232efa3961d896505016c8907bc1f62d93
Abby Luce <abby.c.luce@gmail.com>
2022-05-28 09:40:29 -0600

    Wrote HTML framework. Add CSS file to match comp.

---
+++ main.js
@@ -0,0 +1,16 @@
+var mainHeading = document.querySelector(".self-care-center");
+var subHeading = document.querySelector(".type-of-message");
+var affirmationButton = document.querySelector("#affirmation");
+var mantraButton = document.querySelector("#mantra");
+var receiveMessageButton = document.querySelector(".button");
+var displayReceivedMessage = document.querySelector(".display-received-message");
+var icon = document.querySelector("#icon-1");
+
+// affirmationButton.addEventListener('click', clickAffirmationButton);
+// mantraButton.addEventListener('click', clickMantraButton);
+receiveMessageButton.addEventListener('click', clickReceiveMessageButton);
+
+// function clickReceiveMessageButton() {
+//   icon.classList.add('hidden');
+//   console.log(icon)
+// }
